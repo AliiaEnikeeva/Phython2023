5cfe4ff6c561dfd1e44cfa147fa62c0d7b2a72f4
Enikeeva Aliya <aliaa@mail.ru>
2023-02-04 21:40:38 +0500

    Homework 02

--- Семинар 2/Задача 10.py
+++ Семинар 2/Задача 10.py
@@ -7,3 +7,18 @@
 Input 5 -> 1 0 1 1 0
 Output 2
 '''
+from random import randint
+
+k = 5
+list_coins = []
+for i in range(k):
+    list_coins.append(randint(0, 1))
+print(list_coins)
+zero = list_coins.count(0) # посчитать сколько нулей
+one = len(list_coins) - zero # посчитать сколько единиц
+# print(zero)
+# print(one)
+if one < zero:
+    print(one)
+else:
+    print(zero)
